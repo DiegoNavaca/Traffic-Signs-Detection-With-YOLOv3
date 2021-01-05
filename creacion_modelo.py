60450a9f8773a8ddf4a5d1ae54ff31d1020d269a
Diego <diego.navaca@gmail.com>
2021-01-05 17:28:37 +0100

    Añadir implementacion base

--- creacion_modelo.py
+++ creacion_modelo.py
@@ -158,7 +158,7 @@ class WeightReader:
 # define the model
 model = make_yolov3_model()
 # load the model weights
-weight_reader = WeightReader('yolov3.weights')
+weight_reader = WeightReader('../Weights/yolov3.weights')
 # set the model weights into the model
 weight_reader.load_weights(model)
 # save the model to file
